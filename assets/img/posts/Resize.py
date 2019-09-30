69fb7e099621d2f750aceb40f9a275cf8eb6d9d1
JM Torrente <jmtorrente@uavnavigation.com>
2019-09-30 09:42:31 +0200

    Changed Brownie File extension from jpeg to jpg, also changed python script extensions

--- assets/img/posts/Resize.py
+++ assets/img/posts/Resize.py
@@ -10,22 +10,22 @@ with open((Work + FotoName), 'r+b') as f:
     with Image.open(f) as image:
 
         cover = resizeimage.resize_width(image, 230)
-        cover.save((imName + "_placehold" + '.jpeg'), image.format)
+        cover.save((imName + "_placehold" + '.jpg'), image.format)
 
         cover = resizeimage.resize_width(image, 535)
-        cover.save((imName + "_thumb" + '.jpeg'), image.format)
+        cover.save((imName + "_thumb" + '.jpg'), image.format)
 
         cover = resizeimage.resize_width(image, 1070)
-        cover.save((imName + "_thumb@2x" + '.jpeg'), image.format)
+        cover.save((imName + "_thumb@2x" + '.jpg'), image.format)
 
         cover = resizeimage.resize_width(image, 575)
-        cover.save((imName + "_xs" + '.jpeg'), image.format)
+        cover.save((imName + "_xs" + '.jpg'), image.format)
 
         cover = resizeimage.resize_width(image, 767)
-        cover.save((imName + "_sm" + '.jpeg'), image.format)
+        cover.save((imName + "_sm" + '.jpg'), image.format)
 
         cover = resizeimage.resize_width(image, 991)
-        cover.save((imName + "_md" + '.jpeg'), image.format)
+        cover.save((imName + "_md" + '.jpg'), image.format)
 
         #cover = resizeimage.resize_width(image, 1999)
         #cover.save((imName + "_lg" + '.jpeg'), image.format)
