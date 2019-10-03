44bc6164e811102085d5acc9dee0ff0adc0f6fb0
JM Torrente <jmtorrente@uavnavigation.com>
2019-10-03 12:24:17 +0200

    New changes for easier work

--- assets/img/posts/Compress.py
+++ assets/img/posts/Compress.py
@@ -12,8 +12,8 @@ Casa_Input = 'C:/Users/jmtor/Documents/Projects/Web/assets/img/posts/'
 Work_Input = 'O:/Web/jmtorrente.github.io/assets/img/posts/'
 Casa_Output = 'C:/Users/jmtor/Documents/Projects/Web/assets/img/posts/'
 Work_Output = 'O:/Web/jmtorrente.github.io/assets/img/posts/'
-imName = "Stubby.jpg" #Change depending on the image name
-imName_Res = "Compress.jpg"
+imName = "_Stubby.jpg" #Change depending on the image name
+imName_Res = "Compress"
 
 Im_Input = Work_Input #Change depending on location
 Im_Output = Work_Output #Change depending on location
@@ -25,7 +25,7 @@ print('Input file size   : ', im.size )
 print('Input file name   : ', imName )
 print('Input Image Size  : ', os.path.getsize (Im_Input  + imName))
 
-image_name_output = (imName + imName_Res)
+image_name_output = (imName_Res + imName)
 
 im.save(Im_Output + image_name_output ,optimize=True,quality=50) 
 
