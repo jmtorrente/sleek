65c68bf0aa8e7ed4d721fddd869b3f660e0e105d
ChemaChemos <bourne1988rab@gmail.com>
2019-10-15 19:44:58 +0200

    Compressed front image

--- assets/img/posts/Compress.py
+++ assets/img/posts/Compress.py
@@ -7,12 +7,12 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-FotoName = 'Leica'
+FotoName = 'Yosemite'
 Casa_Input = 'C:/Users/jmtor/Documents/Projects/Web/assets/img/posts/'
 Work_Input = 'O:/Web/jmtorrente.github.io/assets/img/posts/'
 Casa_Output = 'C:/Users/jmtor/Documents/Projects/Web/assets/img/posts/'
 Work_Output = 'O:/Web/jmtorrente.github.io/assets/img/posts/'
-imName = "Leica.jpg" #Change depending on the image name
+imName = "Yosemite.jpg" #Change depending on the image name
 imName_Res = "Compress"
 
 Im_Input = Casa_Input #Change depending on location
