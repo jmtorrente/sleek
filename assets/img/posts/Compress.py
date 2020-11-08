f7bbe7eeeb1e7cf7b22e1e89719774570bab9f52
ChemaChemos <bourne1988rab@gmail.com>
2020-11-08 13:23:01 +0100

    Deleted and added new pictures. Added Easter Egg description. Roll of film box picture to be added

--- assets/img/posts/Compress.py
+++ assets/img/posts/Compress.py
@@ -7,12 +7,12 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-FotoName = 'Courtain_1'
+FotoName = 'Zorki1-3'
 Casa_Input = 'C:/Users/jmtor/Documents/Projects/Web/assets/img/Article_image/Zorki_1_Carrete/'
 Work_Input = 'O:/Web/jmtorrente.github.io/assets/img/posts/'
 Casa_Output = 'C:/Users/jmtor/Documents/Projects/Web/assets/img/posts/'
 Work_Output = 'O:/Web/jmtorrente.github.io/assets/img/posts/'
-imName = "Courtain_1.jpg" #Change depending on the image name
+imName = "Zorki1-3.jpg" #Change depending on the image name
 imName_Res = "Compress_"
 
 Im_Input = Casa_Input #Change depending on location
